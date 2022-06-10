e51be7f56fe0632a742e63621bb3a9139fc1c817
Dakota Kincer <dakota@graffle.io>
2022-06-09 21:46:04 -0400

    added new testnet spork height

--- Graffle.FlowSdk.Services/Nodes/TestNetSporks.cs
+++ Graffle.FlowSdk.Services/Nodes/TestNetSporks.cs
@@ -6,7 +6,7 @@ namespace Graffle.FlowSdk.Services.Nodes
         {
             public const string Name = "TestNet";
             public const string NODE = "access.testnet.nodes.onflow.org:9000";
-            public const ulong ROOT_HEIGHT = 64904846; //TODO new root height!!
+            public const ulong ROOT_HEIGHT = 70072575;
         }
 
         public static class TestNet34
