58da59ffceac457a46f1eab67648a6a8cb0541d6
Ben <ben.adelson@graffle.io>
2022-08-11 09:56:45 -0400

    Feature/test net 37 (#58)
    * add IFlowClientFactory to DI container
    
    * test net spork
    
    * bump version

--- Graffle.FlowSdk.Services/FlowServiceExtensions.cs
+++ Graffle.FlowSdk.Services/FlowServiceExtensions.cs
@@ -11,9 +11,13 @@ namespace Microsoft.Extensions.DependencyInjection
             Console.WriteLine("Set Up Flow Client Factory");
             var nodeName = System.Environment.GetEnvironmentVariable("FlowNode");
             var spork = Sporks.GetSporkByName(nodeName);
+
             Console.WriteLine($"Spork Selected: {spork.Name}");
+
             var flowClientFactory = new FlowClientFactory(nodeName);
-            services.AddSingleton(flowClientFactory);
+            services.AddSingleton(flowClientFactory); //todo remove this eventually?
+            services.AddSingleton(typeof(IFlowClientFactory), flowClientFactory);
+
             Console.WriteLine("Set Up Flow Client Factory Complete");
             return services;
         }
