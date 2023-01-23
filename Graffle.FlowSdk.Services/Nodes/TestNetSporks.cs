e6e3eb6a2acea261a1392374ce1981469c9ec866
Ben Adelson <ben.adelson@graffle.io>
2023-01-23 18:37:37 -0500

    new root height

--- Graffle.FlowSdk.Services/Nodes/TestNetSporks.cs
+++ Graffle.FlowSdk.Services/Nodes/TestNetSporks.cs
@@ -6,7 +6,7 @@ namespace Graffle.FlowSdk.Services.Nodes
         {
             public const string Name = "TestNet";
             public const string NODE = "access.testnet.nodes.onflow.org:9000";
-            public const ulong ROOT_HEIGHT = 90595736; //todo
+            public const ulong ROOT_HEIGHT = 92473965;
         }
 
         public static class TestNet39 //39
