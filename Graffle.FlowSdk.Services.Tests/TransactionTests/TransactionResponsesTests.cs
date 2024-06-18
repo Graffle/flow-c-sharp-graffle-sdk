3ca8e6febc0b085eadec29e92e89608dc7ebdee2
Ben <ben.adelson@graffle.io>
2024-06-18 14:36:41 -0400

    type cast exception refactor (#118)
    * type cast exception refactor
    
    * override message instead of tostring
    
    * minor updates and fix some tests
    
    * bump ver

--- Graffle.FlowSdk.Services.Tests/TransactionTests/TransactionResponsesTests.cs
+++ Graffle.FlowSdk.Services.Tests/TransactionTests/TransactionResponsesTests.cs
@@ -1,5 +1,6 @@
 using Graffle.FlowSdk.Services.Models;
 using Graffle.FlowSdk.Services.Nodes;
+using Graffle.FlowSdk.Services.Serialization;
 using Graffle.FlowSdk.Types;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -142,99 +143,47 @@ namespace Graffle.FlowSdk.Services.Tests.TransactionsTests
         }
 
         [TestMethod]
-        [Ignore] //todo refactor
-        public async Task Transaction_Dictionary_With_UInt64Key()
+        public void Transaction_Dictionary_With_UInt64Key()
         {
-            var transactionResult = await GetTransaction(59886127, "bf49159ac950b2a147eb8df651c1bb125e6c1b202de73ace85d17986a70df9ca");
+            var json = @"{
+                        ""type"": ""Dictionary"",
+                        ""value"": [
+                            {
+                            ""key"": {
+                                ""type"": ""UInt64"",
+                                ""value"": ""123456""
+                            },
+                            ""value"": {
+                                ""type"": ""String"",
+                                ""value"": ""test""
+                            }
+                            }
+                        ]
+                        }";
 
-            var events = transactionResult.Events;
-
-            //get the json we want to verify from the event payload
-            var eventWithArray = events.FirstOrDefault(ev => ev.Type == "A.a4df2ca3e19f32ef.NftItems.Minted");
-            var payload = eventWithArray.Payload;
-            var parsedJson = JsonDocument.Parse(payload);
-            var valueRoot = parsedJson.RootElement.GetProperty("value");
-            var fieldsRoot = valueRoot.GetProperty("fields");
-            var myField = fieldsRoot.EnumerateArray().Skip(1).FirstOrDefault();
-            var dictionary = myField.GetProperty("value");
-            var json = dictionary.GetRawText();
-
-            //use our sdk to parse the json and extract information for verification
-            var parsedType = FlowValueType.CreateFromCadence(json);
-            Assert.IsNotNull(parsedType);
-            Assert.IsInstanceOfType(parsedType, typeof(DictionaryType));
-
-            var dict = parsedType as DictionaryType;
-            var data = dict.Data;
-            Assert.AreEqual(1, data.Keys.Count);
-
-            var key = data.Keys.First();
-            Assert.IsInstanceOfType(key, typeof(UInt64Type));
-            var intKey = key as UInt64Type;
-            Assert.AreEqual((ulong)1, intKey.Data);
-
-            var value = data[key];
-            Assert.IsNotNull(value);
-            Assert.IsInstanceOfType(value, typeof(DictionaryType));
-            var valueDict = value as DictionaryType;
-
-            //this inner dictionary has string keys so not going to do much verification here
-            Assert.AreEqual(3, valueDict.Data.Keys.Count);
+            var res = CadenceJsonInterpreter.ObjectFromCadenceJson(json);
+            Assert.AreEqual("test", res["123456"]);
         }
 
         [TestMethod]
-        [Ignore] //todo refactor
         public async Task Transaction_StructContainsOptionalStruct()
         {
-            var res = await GetTransaction(60816404, "4c6e308af1982760a269c2e44a62dfbea77b62fc36bbacf5e35142ae235dc743");
+            var res = await GetTransaction(80312030, "a586032cc7b3ccab93a68125b4252bbcd31dca8d1545432e6a05b7de5f12c880", NodeType.MainNet);
             var events = res.Events;
-            var ev = events.FirstOrDefault(ev => ev.Type == "A.76b2527585e45db4.SoulMadeMarketplace.SoulMadeForSale");
-            var payload = ev.Payload;
-            var parsedJson = JsonDocument.Parse(payload);
-            var valueRoot = parsedJson.RootElement.GetProperty("value");
-            var fieldsRoot = valueRoot.GetProperty("fields");
-            var myField = fieldsRoot.EnumerateArray().Skip(3).FirstOrDefault();
-            var composite = myField.GetProperty("value");
-            var json = composite.GetRawText();
-
-            var parsed = FlowValueType.CreateFromCadence(json);
-            Assert.IsInstanceOfType(parsed, typeof(CompositeType));
-            var flowStruct = parsed as CompositeType;
-
-            //top level struct
-            //validate fields and id
-            Assert.AreEqual("A.76b2527585e45db4.SoulMadeMarketplace.SoulMadeSaleData", flowStruct.Id);
-            Assert.AreEqual(5, flowStruct.Fields.Count);
-
-            //the last field is an Optional field containing a Struct
-            var optionalStructField = flowStruct.Fields.Last();
-            Assert.AreEqual("componentDetail", optionalStructField.Name);
-
-            //verify it's OptionalType
-            var optionalStructFieldValue = optionalStructField.Value;
-            Assert.IsInstanceOfType(optionalStructFieldValue, typeof(OptionalType));
-
-            //verify the OptionalType contains a Struct Type
-            var optional = optionalStructFieldValue as OptionalType;
-            Assert.IsNotNull(optional.Data);
-            Assert.IsInstanceOfType(optional.Data, typeof(CompositeType));
-
-            //validate inner struct
-            var innerStruct = optional.Data as CompositeType;
-            Assert.AreEqual("A.76b2527585e45db4.SoulMadeComponent.ComponentDetail", innerStruct.Id);
-            Assert.AreEqual(10, innerStruct.Fields.Count);
+            var ev = events.FirstOrDefault(ev => ev.Type == "A.9a57dfe5c8ce609c.SoulMadeMarketplace.SoulMadeForSale");
 
             //let's validate the grafflecomposite
             var graffleComposite = ev.EventComposite;
             var data = graffleComposite.Data;
+            Assert.AreEqual(CadenceSerializerVersion.Expando, graffleComposite.SerializerVersion);
             Assert.IsNotNull(data);
             Assert.AreEqual(4, data.Count);
 
             //pull out the field with a nested struct
-            var nestedFields = data["saleData"]["componentDetail"];
+            var nestedFields = data["saleData"]["mainDetail"]["componentDetails"];
             Assert.IsNotNull(nestedFields);
-            Assert.IsInstanceOfType(nestedFields, typeof(Dictionary<string, object>));
-            Assert.AreEqual(10, nestedFields.Count); //10 fields in the nested struct
+            Assert.IsInstanceOfType(nestedFields, typeof(List<object>));
+            Assert.AreEqual(6, nestedFields.Count); //10 fields in the nested struct
         }
 
         [TestMethod] //backwards compatibility - this txn has the old json structure for Type
